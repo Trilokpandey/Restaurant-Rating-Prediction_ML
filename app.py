adceafaaec8de1317f222868edf5fb89745401b7
trilok <sonu.pandey1990@gmail.com>
2022-06-27 14:15:25 +0530

    added flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from crypt import methods
+from distutils.log import debug
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    return "starting machine learning project"
+
+
+if __name__="__main__":
+    app.run(debug=True)
