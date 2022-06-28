dd69b3fbdeb28340c479f434ab2c142cee096ad3
trilok <sonu.pandey1990@gmail.com>
2022-06-28 13:46:54 +0530

    content updated in app.py file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "starting machine learning project"
+    return "CI CD pipeline has been established."
 
 
 if __name__=="__main__":
